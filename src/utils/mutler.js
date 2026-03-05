17e106ed7e8e2b29c747bf43f35eedf975775e9e
karamo10 <pabimamie@gmail.com>
2026-03-05 08:57:45 +0000

    style(mutler): format code for consistency in multer configuration

--- src/utils/mutler.js
+++ src/utils/mutler.js
@@ -1,6 +1,10 @@
-import multer from "multer";
+import multer from 'multer';
 
 const storage = multer.memoryStorage(); // store file in memory
-const upload = multer({ storage, limits: {fileSize: 2 * 1024 * 1024} }); // limit file size to 2MB
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 2 * 1024 * 1024 },
+}); // limit file size to 2MB
 
 export default upload;
